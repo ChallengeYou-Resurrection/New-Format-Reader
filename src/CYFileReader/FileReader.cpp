c29a68d2bbef663e93b9ee451def5bb3d2e19fc5
Hopson97 <mhopson@hotmail.co.uk>
2018-12-26 21:56:34 +0000

    Begin work on reading file

--- src/CYFileReader/FileReader.cpp
+++ src/CYFileReader/FileReader.cpp
@@ -1,12 +1,31 @@
 #include "FileReader.h"
 
+#include <iostream>
+
 #include "../Utilities.h"
 #include "BinaryFileReader.h"
+
+
+namespace {
+    template <typename T>
+    void print(const char* msg, const T& val) {
+        std::cout << msg << ": <" << val << ">\n";
+    }
+} 
+
+
 
 CYLevel loadLevel(const std::string& file) {
     CYLevel level;
     auto data = readFileIntoVec(file);
     BinaryFileReader reader (std::move(data));
+
+    uint8_t fileVersion;
+    reader >> fileVersion >> level.levelName >> level.creatorName;
+
+    print("Version", (int)fileVersion);
+    print("Game Name", level.levelName);
+    print("Game Author", level.creatorName);
 
     return level;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
+#include "CYFileReader/FileReader.h"
 
 int main() {
-    
+    auto level = loadLevel("../../games/GlassTower2[258].cyb");    
 }

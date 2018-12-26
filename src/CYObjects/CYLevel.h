db7e8b33123d5675f6e1f0d2f93ea2c54e5242e8
Hopson97 <mhopson@hotmail.co.uk>
2018-12-26 22:01:36 +0000

    Add more metadata to CYLevel struct

--- src/CYObjects/CYLevel.h
+++ src/CYObjects/CYLevel.h
@@ -9,6 +9,10 @@
 struct CYLevel {
     std::string levelName;
     std::string creatorName;
+    uint8_t numFloors;
+    uint8_t music;
+    uint8_t theme;
+    uint8_t weather;
 
-    std::array<std::vector<CYObject>, (int)ObjectID::NUM_OBJECTS> objects;
+    std::array<std::vector<CYObject>, (size_t)ObjectID::NUM_OBJECTS> objects;
 };

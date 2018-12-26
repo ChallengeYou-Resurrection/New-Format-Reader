2b4c345b001e80bbee2512360d2962959fd4b1ba
Hopson97 <mhopson@hotmail.co.uk>
2018-12-26 21:56:17 +0000

    Add reader function to buffer reader

--- src/CYFileReader/BinaryFileReader.cpp
+++ src/CYFileReader/BinaryFileReader.cpp
@@ -2,4 +2,6 @@
 
 BinaryFileReader::BinaryFileReader(std::vector<unsigned char>&& data) 
 :   m_buffer    (data) 
-{}
+{   
+    m_readPtr = m_buffer.data();
+}
--- src/CYFileReader/BinaryFileReader.h
+++ src/CYFileReader/BinaryFileReader.h
@@ -1,11 +1,55 @@
 #pragma once
 
 #include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdint>
+#include <byteswap.h>
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+
+using i16 = int16_t;
 
 class BinaryFileReader {
     public:
         BinaryFileReader(std::vector<unsigned char>&& data);
 
+        friend BinaryFileReader& operator >>(BinaryFileReader& reader, u8& n) {
+            reader.readNumber(n);
+            return reader;
+        }
+
+        friend BinaryFileReader& operator >>(BinaryFileReader& reader, u16& n) {
+            reader.readNumber(n);
+            n = bswap_16(n);
+            return reader;
+        }
+
+        friend BinaryFileReader& operator >>(BinaryFileReader& reader, u32& n) {
+            reader.readNumber(n);
+            return reader;
+        }
+
+        friend BinaryFileReader& operator >>(BinaryFileReader& reader, std::string& str) {
+            uint32_t length = 0;
+            reader >> length;
+            str.assign(reader.m_readPtr, reader.m_readPtr + length);
+            reader.m_readPtr += length;
+            return reader;
+        }
+
     private:
+        template<typename T>
+        void readNumber(T& n) {
+            std::memcpy(reinterpret_cast<char*>(&n), m_readPtr, sizeof(T));
+            m_readPtr += sizeof(T);
+        }
+
         std::vector<unsigned char> m_buffer;
+        unsigned char* m_readPtr;
 };
+
+BinaryFileReader& operator >>(BinaryFileReader& reader, u8& n);

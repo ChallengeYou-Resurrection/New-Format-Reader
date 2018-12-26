69e79d0a038e02e795ef34b39a6ec40ced43e3e2
Hopson97 <mhopson@hotmail.co.uk>
2018-12-26 20:36:41 +0000

    Create utilities funtion

---
+++ src/Utilities.cpp
@@ -0,0 +1,26 @@
+#include "Utilities.h"
+
+#include <iterator>
+#include <fstream>
+#include <iostream>
+
+std::vector<unsigned char> readFileIntoVec(const char* filename) {
+    std::ifstream inFile(filename, std::ios::binary);
+    if (!inFile.is_open()) {
+        std::cerr << "Unable to open " << filename << '\n';
+        exit(1);
+    }
+    inFile.unsetf(std::ios::skipws);
+    std::streampos fileSize;
+    inFile.seekg(0, std::ios::end);
+    fileSize = inFile.tellg();
+    inFile.seekg(0, std::ios::beg);
+    std::vector<unsigned char> vec;
+    vec.reserve(fileSize);
+
+    vec.insert(vec.begin(),
+               std::istream_iterator<unsigned char>(inFile),
+               std::istream_iterator<unsigned char>());
+
+    return vec;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,4 @@
-#include <iostream>
-#include <fstream>
-
-#include "CYObjects/CYLevel.h"
 
 int main() {
-    std::string fileName = "GlassTower2[258].cyb";
-    std::ifstream inFile("../../games/" + fileName, std::ifstream::binary);
-    if (!inFile.is_open()) {
-        std::cerr << "Unable to open " << fileName << '\n';
-        return -1;
-    }
+    
 }

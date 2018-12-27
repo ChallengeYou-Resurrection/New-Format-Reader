4e1a82d10f9f2979458f5f1521855a39ca11d7f7
Hopson97 <mhopson@hotmail.co.uk>
2018-12-27 09:13:27 +0000

    Add CYWall struct

---
+++ src/CYObjects/CYObject.cpp
@@ -0,0 +1,11 @@
+#include "CYObject.h"
+
+#include "../CYFileReader/BinaryFileReader.h"
+
+void Position::deserialize(BinaryFileReader& buffer) {
+    buffer >> x >> z;
+}
+
+void CYWall::deserialize(BinaryFileReader& buffer) {
+    buffer  >> beginPoint >> endPoint >> floor;
+}
--- src/CYObjects/CYObject.h
+++ src/CYObjects/CYObject.h
@@ -2,6 +2,25 @@
 
 #include <variant>
 
+class BinaryFileReader;
+
+struct Position {
+    int16_t x = 0;
+    int16_t z = 0;
+
+    void deserialize(BinaryFileReader& buffer);
+};
+
+
 struct CYObject {
 
 };
+
+struct CYWall {
+    Position beginPoint;
+    Position endPoint;
+    std::vector<std::string> properties;
+    uint8_t floor;
+
+    void deserialize(BinaryFileReader& buffer);
+}
